4ed68236411c8411525c68451b68d4199559c359
Serneum <serneum@gmail.com>
2016-05-23 15:03:53 -0400

    Fix rendering of tasks. Add route to edit tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,22 @@
 class TasksController < ApplicationController
+
+  def index
+    @task = Task.new
+  end
+
   def create
     @task = current_user.tasks.create(task_params)
-    redirect_to tasks_path(current_user)
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
     @task = current_user.tasks.find(params[:id])
     if @task.update(task_params)
-      redirect_to tasks_path(current_user)
+      redirect_to tasks_path
     else
       render 'edit'
     end
